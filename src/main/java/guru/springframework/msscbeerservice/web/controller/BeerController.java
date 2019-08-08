5564ad75b0b25ab5ea3b857080d8d9081a9ce8f9
subhajit chatterjee <Subhajit.Chatterjee88@gmail.com>
2019-08-08 22:06:31 +0530

    Adding Controller Classes. Commit Number 3

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -23,7 +23,7 @@ public class BeerController {
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId , @PathVariable BeerDto beerDto){
+    public ResponseEntity updateBeerById(@PathVariable("beerId")UUID beerId , @RequestBody BeerDto beerDto){
 
         return new ResponseEntity(HttpStatus.NO_CONTENT) ;
     }
